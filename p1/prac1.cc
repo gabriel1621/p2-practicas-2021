d7e94f88bef6588b467370d6ee408ca1967f7d98
gpr11-ua <gpr11@alu.ua.es>
2022-02-17 18:45:29 +0100

    17/02/2022

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -43,64 +43,169 @@ int rollDice(){   //funcion de dado
 int nombreheroe(Hero &name){};
 int attack_defense(){};
 
+
 Hero createHero(Hero &name ){
   nombreheroe(Hero.name);{  //funcion nombre heroe
     cout << "Enter Hero name: \n ";
-    cin >> Hero.name;
-    if (Hero.name = " ");{ //prueba de nombre vacio
+    char nombre_Heore;
+    cin >> nombre_Heore;
+    if (nombre_Heore = "");{ //prueba de nombre vacio
       cout << "Error Worng Name \n";
       nombreheroe();
     };
-    else (isalpha(Hero.name [0]));{ //prueba valores alfanumericos
+    else-if ((isalnum(nombre_Heore[0]) == 0));{ //prueba valores alfanumericos
       cout << "Error Worng Name \n";
       nombreheroe();
     };
+    else{
+      Hero.name=nombre_Heore;
+    }
     
 
-  }/*
+  }
   attack_defense();{ //funcion ataque y defensa
     int ataque_defensa;
     cout << "Enter attack/defense \n";
     cin >> ataque_defensa;
-      //trocear el parametro en dos valores enteros (%ataque/%defensa)
+      //cin.get()   trocear el parametro en dos valores enteros (%ataque/%defensa)
       //comprobar valores > 0
     if (ataque+defesensa=100); { //comprobar que los dos valores suman 100
       hero.Core.attack = (ataque/100)*200;
       hero.Core.defense = (defensa/100)*200;
 
-    }  
-    else{ cout << "Error Worng Distribution"; attack_defense();}
+    } ;
+    else{ 
+      cout << "Error Worng Distribution"; 
+      attack_defense();
+    };
     
   }
   Hero.core.hp = (hero.Core.defense*2);
   Hero.special = true;
   Hero.exp = 0;
   Hero.kills = 0;
+  Hero.runaways = 3;
 }
 
-Enemy createEnemy(){
+Enemy createEnemy(Enemy &enemy){
   switch (rollDice){
     if rollDice <=6 {
-      Enemy.name="AXOLOTL";}
+      cout  << "Breed: ",Enemy.Breed= 1;
+            << "Attack: ",Enemy.Core.attack= 40 ;
+            << "Defense: ",Enemy.Core.defense= 40;
+            << "Health points: ",Enemy.core.hp = (hero.Core.defense*2); }
       
-    else rollDice <=11 {
-      Enemy.name="TROLL";}
+    else-if rollDice <=11 {
+      cout  << "Breed: ",Enemy.Breed= 2;
+            << "Attack: ",Enemy.Core.attack= 60 ;
+            << "Defense: ",Enemy.Core.defense= 80;
+            << "Health points: ",Enemy.core.hp = (hero.Core.defense*2);}
       
-    else rollDice <=15 {
-      Enemy.name="ORC";}
+    else-if rollDice <=15 {
+      cout  << "Breed: ",Enemy.Breed= 3;
+            << "Attack: ",Enemy.Core.attack= 80 ;
+            << "Defense: ",Enemy.Core.defense= 120;
+            << "Health points: ",Enemy.core.hp = (hero.Core.defense*2);}
       
-    else rollDice <=18 {
-      Enemy.name="HELLHOUND";}
+    else-if rollDice <=18 {
+      cout  << "Breed: ",Enemy.Breed= 3;
+            << "Attack: ",Enemy.Core.attack= 120 ;
+            << "Defense: ",Enemy.Core.defense= 100;
+            << "Health points: ",Enemy.core.hp = (hero.Core.defense*2);}
       
-    else rollDice <=20 {
-      Enemy.name="DRAGON";}
+    else-if rollDice <=20 {
+     cout  << "Breed: ",Enemy.Breed= 3;
+            << "Attack: ",Enemy.Core.attack= 160 ;
+            << "Defense: ",Enemy.Core.defense= 140;
+            << "Health points: ",Enemy.core.hp = (hero.Core.defense*2);}
       
   }
 }
+/*Inicializacion de funciones*/
+report();
+showMenu();
+/********************/
+void fight(Hero &hero,Enemy &enemy){   
 
-void fight(Hero &hero,Enemy &enemy){
+  do{
+    int dadoheore = rollDice();
+      dadoheore = (dadoheore*5);
+    int dadoenemy = rollDice();
+      dadoenemy = (dadoenemy*5);
+    int vida_inicial = Hero.core.attack;
+      Hero.core.attack= (dadoheore+vida_inicial);
+    int defensa_inicial = Enemy.core.defense;
+      Enemy.core.defense= (dadoenemy+defensa_inicial);
+    int hit_points;
+      hit_points = (Enemy.defense-Hero.attack);
+      Enemy.core.hp= (Hero.core.hp-hit_points);
+    cout << "[Hero -> Enemy]" << endl
+         << "Attack: ",vida_inicial,"+",dadoheore << endl
+         << "Defense: ",defensa_inicial,"+",dadoenemy << endl
+         << "Hit Point: ", hit_points << endl
+         << "Enemy health points: ",Enemy.core.hp << endl
+    if (Enemy.core.hp<=0){
+      break;
+    }
+    else{
+      int dadoenemy = rollDice();
+      dadoenemy = (dadoenemy*5); 
+      int dadoheore = rollDice();
+      dadoheore = (dadoheore*5);
+
+    int vida_inicial = Enemy.core.attack;
+      Enemy.core.attack= (dadoheore+vida_inicial);
+    int defensa_inicial = Hero.core.defense;
+      Hero.core.defense= (dadoenemy+defensa_inicial);
+    int hit_points;
+      hit_points = (Hero.defense-Enemy.attack);
+      Hero.core.hp= (Enemy.core.hp-hit_points);
+    cout << "[Enemy -> Hero]" << endl
+         << "Attack: ",vida_inicial,"+",dadoenemy << endl
+         << "Defense: ",defensa_inicial,"+",dadoheore << endl
+         << "Hit Point: ", hit_points << endl
+         << "Enemy health points: ",Hero.core.hp << endl
+    if Hero.core.hp>0 {
+      showMenu();
+    }
+    else{
+      cout << "You are dead";
+      report ();
+    }
+    }
+  }
+  while (Enemy.core.hp<=0){
+    cout << "Enemy Killed" << endl
+    if enum.Breed = 1 {
+      Hero.exp = Hero.exp+100;
+    }
+    else-if enum.Breed = 2 {
+      Hero.exp = Hero.exp+150;
+    }
+    else-if enum.Breed = 3 {
+      Hero.exp = Hero.exp+200;
+    }
+    else-if enum.Breed = 4 {
+      Hero.exp = Hero.exp+300;
+    }
+    else-if enum.Breed = 5 {
+      Hero.exp = Hero.exp+400;
+    }
+
+  }
+
 }
-
+void runawayfuncion(Hero &hero){
+  int contador_de_huida;
+  contador_de_huida ++;
+ while Hero.runaways>=0 || contador_de_huida<=2{
+   Hero.runaways= Hero.runaways-1;
+   createEnemy();
+ }
+ cout << "ERROR: cannot run away";
+ showMenu();
+  
+}
 void report(const Hero &hero){
 }
 
@@ -114,11 +219,11 @@ void showMenu(){
        << "Option: ";
   cin >> opcion; 
   switch (opcion){
-    case 1:
-    case 2:
+    case 1: fight();
+    case 2: runawayfuncion();
     case 3:
-    case 4:
-    case "q":
+    case 4: report();
+    case "q": Exit;
     default: 
       cout << "Error Wrong Option \n";
       showMenu()
@@ -133,6 +238,8 @@ int main(int argc,char *argv[]){
     srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
     
     // Aquí vendrá todo tu código del "main"...
+    createHero();
+    createEnemy();
+    showMenu();
   }
 }
-*/
