addf9c7398ba3bce5baae4830f4db9c97953d758
gpr11-ua <gpr11@alu.ua.es>
2022-03-03 18:40:14 +0100

    03/03/2022

--- p2/prac2.cc
+++ p2/prac2.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
@@ -84,20 +86,154 @@ void showMainMenu() {
        << "q- Quit" << endl
        << "Option: ";
 }
-
+/*mostrará un listado con información resumida de los libros del catálogo*/
 void showCatalog(const BookStore &bookStore) {
+  int canti_libros=sizeof(books);
+  for (int a=0;a<=canti_libros;a++){
+    cout<< book.id<<". "<<book.title<<" "<<book.year<<", "<<book.price<<endl;
+  }
 }
 
 void showExtendedCatalog(const BookStore &bookStore) {
+  int canti_libros=sizeof(books);
+  for (int a=0;a<=canti_libros;a++){
+    cout << "book.title \"" 
+         <<","
+         <<"book.author \""
+         <<","
+         <<book.year
+         <<","
+         <<"book.slug \""
+         <<","
+         <<book.price<<endl;
+  }
 }
+bool control_error(BookStore &bookStore){
+  return control_error;
+}
+void create_slug(BookStore &bookStore){
+  string nuevo_slug=book.title;
+  recorrido=strlen(nuevo_slug);
 
-void addBook(BookStore &bookStore) {
+  for(int w=0;w<recorrido;w++){
+    nuevo_slug[w]=tolower(nuevo_slug[w]);//convertir a minusculas
+  }
+
+  for(int r=0;r<recorrido;r++){
+    if(isalnum(nuevo_slug[r])=!0){
+       nuevo_slug[r]='-';   }          //convertir caracters especiales  a "-"
+  }
+
+  for(int t=0;t<recorrido;t++){
+    if ((nuevo_slug[t] =='-') && (nuevo_slug[t+1] == '-')){
+
+    }
+    //eliminar "-" seguidos
+  }
+
+}
+void addBook(BookStore &bookStore, Book &book) {
+  bool error,error2,error3,error4;
+  int longitud=0;
+  /*nombre*/
+  do{
+    cout << "Enter book title:";
+    cin.getline(book.title,KMAXSTRING-1)
+    error=false
+
+    longitud = strlen(book.title);
+    for (int i=0;i<longitud;i++){
+      if((isalnum(book.title[i])==0) && (book.title[i]!=' ') 
+          && (book.title[i]!=',') && (book.title[i]!=':') 
+          && (book.title[i]!='-')&& (book.title[i]!='\0')){
+        error(ERR_BOOK_TITLE);
+        error=true;
+      }
+    }
+    if (longitud==0){
+      error(ERR_BOOK_TITLE);
+      error=true;
+    }
+    
+  }while(error==false);
+  /*autor*/
+  do{
+    cout << "Enter author(s):";
+    cin.getline(book.authors,KMAXSTRING-1)
+    error2=false
+
+    longitud = strlen(book.authors);
+    for (int i=0;i<longitud;i++){
+      if((isalnum(book.authors[i])==0) && (book.authors[i]!=' ') 
+          && (book.authors[i]!=',') && (book.authors[i]!=':') 
+          && (book.authors[i]!='-')&& (book.authors[i]!='\0')){
+        error(ERR_BOOK_TITLE);
+        error2=true;
+      }
+    }
+    if (longitud==0){
+      error(ERR_BOOK_AUTHORS);
+      error2=true;
+    }
+    
+  }while(error2==false);
+  /*año*/
+  do{
+    int year=0;
+    int longitud=0;
+    cout <<"Enter publication year:";
+    cin.getline(year);
+
+    if((book.year<1440) && (book.year>2022)){
+      error(ERR_BOOK_DATE);
+      error3=true;
+    }
+    book.year = stoi(year);//convertir enetero a string
+    longitud=strlen(book.year);
+    if (longitud==0){
+      error(ERR_BOOK_DATE);
+      error3=true;
+    }
+
+  }while(error3==false);
+  /*precio*/
+  do{
+    float price=0;
+    int longitud=0;
+    cout <<"Enter price:";
+    cin.getline(price);
+
+    if(price<=0){
+      error(ERR_BOOK_PRICE);
+      error4=true;
+    }
+    bookStore.price = stof(year);//convertir enetero a string
+    longitud=strlen(book.price);
+    if (longitud==0){
+      error(ERR_BOOK_PRICE);
+      error4=true;
+    }
+
+  }while(error4==false);
+  create_slug(bookStore);
 }
 
 void deleteBook(BookStore &bookStore) {
+  cout << "Enter book id: " << endl;
+  cin.getline(BinBook.id);
+
+
 }
 
 void importExportMenu(BookStore &bookStore) {
+  cout << "[Import/export options]" << endl
+       <<"1- Import from CSV"<< endl
+       <<"2- Export to CSV"<< endl
+       <<"3- Load data"<< endl
+       <<"4- Save data"<< endl
+       <<"b- Back to main menu"<< endl
+       <<"Option: ";
+
 }
 
 void importFromCsv(BookStore &bookStore){
@@ -114,6 +250,7 @@ void saveData(const BookStore &bookStore){
 
 int main(int argc, char *argv[]) {
   BookStore bookStore;
+  Book book;
   bookStore.name = "My Book Store";
   bookStore.nextId = 1;
 
@@ -131,7 +268,7 @@ int main(int argc, char *argv[]) {
         showExtendedCatalog(bookStore);
         break;
       case '3':
-        addBook(bookStore);
+        addBook(bookStore,book);
         break;
       case '4':
         deleteBook(bookStore);
